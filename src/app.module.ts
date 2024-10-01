cd9f03b48733057f803f85e42f2161e105d1921d
andreesilva <andre.erminio@gmail.com>
2024-10-01 01:28:19 -0300

    Adicionando e configurando o prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
+import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
-  imports: [ConfigModule, EnvConfigModule, UsersModule],
+  imports: [ConfigModule, EnvConfigModule, UsersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
