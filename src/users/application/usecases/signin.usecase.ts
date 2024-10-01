e813c0834a28bcc33dd950ca7c52fba7e6731b01
andreesilva <andre.erminio@gmail.com>
2024-09-30 22:42:04 -0300

    Registrando o SignupUseCase

--- src/users/application/usecases/signin.usecase.ts
+++ src/users/application/usecases/signin.usecase.ts
@@ -1,6 +1,6 @@
 import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { BadRequestError } from '@/shared/application/errors/bad-request-error';
-import { UserEntity } from '@/users/domain/entities/user.entity';
+
 import { UserRepository } from '@/users/domain/repositories/user.repositoriy';
 import { UserOutput, UserOutputMapper } from '../dto/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,36 @@
+import { BcryprjsHashProvider } from './providers/has-provider/bcryptjs-hash.provider';
+
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository';
+import { SigninUseCase } from '../application/usecases/signin.usecase';
+import { UserRepository } from '../domain/repositories/user.repositoriy';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UsersRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryprjsHashProvider,
+    },
+    {
+      provide: SigninUseCase.UseCase,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SigninUseCase.UseCase(userRepository, hashProvider);
+      },
+
+      inject: ['UsersRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
