a2bf056df19d112c5d95062695cf4cbef7734aa7
andreesilva <andre.erminio@gmail.com>
2024-10-02 19:19:13 -0300

    Instalando a biblioteca swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,35 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs com Clear Arch')
+    .setDescription(
+      'Criando uma API NestJS com Arquitetura Limpa, DDD, e automação de testes',
+    )
+    .setVersion('1.0')
+    .addBearerAuth({
+      description: 'Informar o token JWT para autenticar',
+      name: 'authorization',
+      scheme: 'bearer',
+      type: 'http',
+      in: 'header',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
