a53b6c1e921e386fbb054eb7a9420451b614c110
andreesilva <andre.erminio@gmail.com>
2024-10-02 03:22:10 -0300

    Implementando o AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { ConfigModule } from '@nestjs/config';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
+import { AuthModule } from './auth/infrastructure/auth.module';
 
 @Module({
-  imports: [ConfigModule, EnvConfigModule, UsersModule, DatabaseModule],
+  imports: [
+    ConfigModule,
+    EnvConfigModule,
+    UsersModule,
+    DatabaseModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
