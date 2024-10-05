e49e3bfa14123fad1e877d95ab1b321c050ce188
andreesilva <andre.erminio@gmail.com>
2024-10-05 15:02:39 -0300

    Editando importação no arquivo ClassValidatorFields

---
+++ jest.int.config.ts
@@ -0,0 +1,17 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  moduleFileExtensions: ['js', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  //testRegex: '.*\\.spec\\.ts$',
+  testRegex: '.*\\.int-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
